2dc7fd32d5fa37d6dfea2ad0dc026a0b78a38f43
Gunoo Kim <73500662+SkGunoo@users.noreply.github.com>
2025-03-14 17:48:02 +0000

    update right_shift_letter method -the method shift given lettr by specified value -it handles both uppercase and lowercase -returns non alphabet characters without modification -letter will wrap around within the alphabet(     shifting z by 1 will become a )

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -28,11 +28,55 @@
 
   end
 
-  def rifght_shift_letter(letter)
+  def right_shift_letter(letter,shift_value)
+    #check if its alphabet 
+    lowercase = ('a'..'z')
+    uppercase = ('A'..'Z')
+    #return non alphabet
+    return letter unless lowercase.include?(letter) || uppercase.include?(letter)
+
+    #check case 
+
+    is_lowercase = false
+
+    is_lowercase = true if lowercase.include?(letter) 
+
+    
+    letter_code = letter.downcase.ord
+    shifted_value = letter_code + shift_value
+
+    if shifted_value > 122
+      # 96 because 97 is 'a'. if its 97 it adds one more shift
+      if is_lowercase
+        return (96 + (shifted_value - 122)).chr
+
+      else
+        return (96 + (shifted_value - 122)).chr.upcase
+
+      end
+
+    else
+      if is_lowercase
+        return shifted_value.chr
+      else
+        #change case if its not lowercase 
+        return shifted_value.chr.upcase
+      end
+    end
+  
+
+    
+
+    # z = 122 
+    # a = 97
+
+
   end
 
   def caesar_cipher(phrase, shift_value)
+
   end
 
+  
 
   
